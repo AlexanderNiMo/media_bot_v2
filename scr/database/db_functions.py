193727ba37b7f28931c52a0621762e75c20bd93b
alex <binipox@gmail.com>
2018-10-22 17:20:13 +0300

    Добавление функции обновления факта запроса медиа. Испарвлеине поведения функций добавления медиа.

--- scr/database/db_functions.py
+++ scr/database/db_functions.py
@@ -25,7 +25,8 @@ class DbManager:
             try:
                 self.__enj = init_db(self.__get_connection_str(), self.config.DATABASE_NAME)
             except OperationalError:
-                self.__enj = create_db(self.__get_connection_str(), self.config.DATABASE_NAME)
+                create_db(self.__get_connection_str(), self.config.DATABASE_NAME)
+                self.__enj = init_db(self.__get_connection_str(), self.config.DATABASE_NAME)
         return self.__enj
 
     @property
@@ -35,7 +36,9 @@ class DbManager:
         return self.__session
 
     def close_session(self):
-        self.session.close()
+        if self.__session is None:
+            return
+        self.__session.close()
         self.__session = None
 
     def get_users_for_notification(self):
@@ -107,13 +110,41 @@ class DbManager:
         # self.close_session()
         return data
 
-    def add_film(self, kinopoisk_id, label, year, url):
+    def update_media_params(self, media_id: int, params: dict, media_type):
+
+        find_dict = {
+            'kinopoisk_id': media_id,
+            'media_type': media_type,
+
+        }
+
+        if 'season' in params.keys():
+            find_dict.update({
+                'season': params['season']
+            })
+
+        media = self.find_media(**find_dict)
+
+        if media is None:
+            raise AttributeError('По kinopoisk_id {} не существует фильма для обновления.'.format(media_id))
+
+        for key in params.keys():
+            setattr(media, key, params[key])
+
+        self.session.add(media)
+        self.session.commit()
+        self.close_session()
+
+    def add_film(self, client_id, kinopoisk_id, label, year, url):
         film = self.Film(kinopoisk_id=kinopoisk_id, label=label, year=year, kinopoisk_url=url)
+        user = self.find_user(client_id)
+        user.media.append(film)
         self.session.add(film)
+        self.session.add(user)
         self.session.commit()
         return film
 
-    def add_serial(self, kinopoisk_id, label, year, season, url, max_series=0):
+    def add_serial(self, client_id, kinopoisk_id, label, year, season, url, max_series=0):
         serial = self.Serial(
             kinopoisk_id=kinopoisk_id,
             label=label,
@@ -121,8 +152,10 @@ class DbManager:
             season=season,
             series=max_series,
             kinopoisk_url=url)
-
+        user = self.find_user(client_id)
+        user.media.append(serial)
         self.session.add(serial)
+        self.session.add(user)
         self.session.commit()
         return serial
 
@@ -131,6 +164,15 @@ class DbManager:
         self.session.add(user)
         self.session.commit()
         return user
+
+    def add_media_to_user_list(self, client_id, kinopoisk_id, media_type, season):
+        media = self.find_media(kinopoisk_id, media_type, season)
+        user = self.find_user(client_id)
+        if media not in user.media:
+            user.media.append(media)
+        self.session.add(media)
+        self.session.add(user)
+        self.session.commit()
 
     def change_user_option(self, clien_id, option_name: UserOptions, value):
 
