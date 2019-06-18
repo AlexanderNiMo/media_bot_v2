eb698e6de2c9d4ec207b9c7f3a8a1b83c41a026a
alex <binipox@gmail.com>
2019-06-18 17:30:05 +0300

    Добавление механизма выбора торрент лиента.

--- src/crawler/Workers/TorrentClientWorker.py
+++ src/crawler/Workers/TorrentClientWorker.py
@@ -219,7 +219,7 @@ class QBitTorrent(TorrentWorker):
 
 def get_torrent_worker(job, data_config):
 
-    torrent_client_type = data_config.TORRENT_TYPE
+    torrent_client_type = int(data_config.TORRENT_TYPE)
 
     if torrent_client_type == 0:
         return DelugeWorker(job, data_config)
